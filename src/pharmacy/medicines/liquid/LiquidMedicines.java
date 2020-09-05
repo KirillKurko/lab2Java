3ef6d2bb26145908f757380df4bbb3bd5842bdc8
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-05 15:00:46 +0300

    LiquidMedicines class implemented

--- src/pharmacy/medicines/liquid/LiquidMedicines.java
+++ src/pharmacy/medicines/liquid/LiquidMedicines.java
@@ -1,4 +1,40 @@
 package pharmacy.medicines.liquid;
 
-public class LiquidMedicines {
+import pharmacy.medicines.Medicine;
+
+abstract public class LiquidMedicines extends Medicine {
+    protected int volume;
+
+    public LiquidMedicines() {
+        super();
+        volume = 0;
+    }
+
+    public LiquidMedicines(String name, float cost, int volume) {
+        super(name, cost);
+        this.volume = volume;
+    }
+
+    public LiquidMedicines(LiquidMedicines liquidMedicines) {
+        super(liquidMedicines);
+        volume = liquidMedicines.volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    protected String generateType() {
+        return "liquid medicines";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nVolume: " + volume;
+    }
 }
