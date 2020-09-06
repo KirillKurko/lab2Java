1e6c8260c342c9fada6f6427589beaec90ab593f
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-06 11:16:16 +0300

    Drops class implemented

--- src/pharmacy/medicines/liquid/Drops.java
+++ src/pharmacy/medicines/liquid/Drops.java
@@ -1,4 +1,66 @@
 package pharmacy.medicines.liquid;
 
-public class Drops {
+import java.util.Objects;
+
+public class Drops extends  LiquidMedicines {
+    private String applicationMode;
+    private static int count = 0;
+    private int id;
+
+    public Drops() {
+        super();
+        applicationMode = "";
+        ++count;
+        id = count;
+    }
+
+    public Drops(String name, float cost, int volume, String applicationMode) {
+        super(name, cost, volume);
+        this.applicationMode = applicationMode;
+        ++count;
+        id = count;
+    }
+
+    public Drops(Drops drops) {
+        super(drops);
+        applicationMode = drops.applicationMode;
+        ++count;
+        id = count;
+    }
+
+    public String getApplicationMode() {
+        return applicationMode;
+    }
+
+    public void setApplicationMode(String applicationMode) {
+        this.applicationMode = applicationMode;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nApplication mode: " + applicationMode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Drops drops = (Drops) obj;
+        return id == drops.id &&
+                applicationMode.equals(drops.applicationMode) &&
+                volume == drops.volume &&
+                name.equals(drops.name) &&
+                type.equals(drops.name) &&
+                cost == drops.cost &&
+                price == drops.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(applicationMode, id);
+    }
 }
